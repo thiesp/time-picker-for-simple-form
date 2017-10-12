cc4dd68fba527ee08182296b6904157de357108e
Thies Pierdola <tp@centic.dk>
2017-10-12 10:31:15 +0200

    loading dependencies

--- lib/time_wrapper.rb
+++ lib/time_wrapper.rb
@@ -5,3 +5,7 @@ require "time_wrapper/locale/locale"
 require "time_wrapper/simple_form_wrapper"
 require "time_wrapper/engine"
 
+require "font-awesome-rails"
+require "bootstrap"
+require "momentjs-rails"
+require "bootstrap3-datetimepicker-rails"
