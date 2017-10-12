bd01e37a10e35d5a2d3ebdd3ba00de449d0657d3
Thies Pierdola <tp@centic.dk>
2017-10-12 10:30:53 +0200

    moving engine into its own file

--- lib/time_wrapper.rb
+++ lib/time_wrapper.rb
@@ -3,12 +3,5 @@ require "time_wrapper/version"
 require "time_wrapper/orm/active_record"
 require "time_wrapper/locale/locale"
 require "time_wrapper/simple_form_wrapper"
-
-module TimeWrapper
-  module Rails
-    class Engine < ::Rails::Engine
-
-    end
-  end
-end
+require "time_wrapper/engine"
 
